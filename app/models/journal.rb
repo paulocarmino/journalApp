33b872c177486b671edd9418f053ea1362b9bf0d
Paulo Carmino <contato@paulocarmino.com>
2018-09-25 09:45:51 -0300

    Add journal model create morning and question method

--- app/models/journal.rb
+++ app/models/journal.rb
@@ -3,4 +3,15 @@ class Journal < ApplicationRecord
     has_one :morning
     has_one :night
     has_many :questions
+
+    def self.create_with_morning_and_night user
+        journal = nil
+        ActiveRecord::Base.transaction do
+            journal = Journal.create(user: user)
+            journal.create_morning
+            journal.create_night
+        end
+        journal
+    end
+
 end
